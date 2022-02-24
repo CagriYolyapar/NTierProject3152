7e158d4e3a22fb43f7033b293c92844389c2dee6
Cagri Yolyapar <cagri.yolyapar@gmail.com>
2022-02-24 11:09:10 +0300

    MVCUI katmanına baslandı...Auth sınıfları ve ShoppingTools sınıfları acıldı

---
+++ Project.MVCUI/ConsumeDTO/PaymentDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Project.MVCUI.ConsumeDTO
+{
+    public class PaymentDTO
+    {
+        //Sanal Pos Entegrasyonu
+
+        //Normalde bu tarz sınıflar calıstıgınız ilgili yerden aldıgınız dökümantasyonların kılavuzlugu sayesinde olusturulur...
+    }
+}
